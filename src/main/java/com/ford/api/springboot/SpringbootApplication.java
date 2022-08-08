db6ce42c1db2dd8d086c294a262345ba4e2d1165
Tiago <lima.tiagom@gmail.com>
2022-08-07 23:03:19 -0300

    Allow cors

--- src/main/java/com/ford/api/springboot/SpringbootApplication.java
+++ src/main/java/com/ford/api/springboot/SpringbootApplication.java
@@ -3,12 +3,25 @@ package com.ford.api.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8080", "http://localhost:4200");
+			}
+		};
 	}
 
 }
